fe2cb5bab13d23c20c8f891999b6528f5a41ea4f
Johannes Egger <j.egger@posteo.at>
2018-08-11 22:12:23 +0200

    Make Lageplan map responsive

--- src/Client/Lageplan/View.fs
+++ src/Client/Lageplan/View.fs
@@ -10,6 +10,6 @@ let root =
       iframe
         [ Src "https://www.google.com/maps/embed?pb=!1m17!1m8!1m3!1d753.3551123114898!2d13.7883404!3d47.9512645!3m2!1i1024!2i768!4f13.1!4m6!3e0!4m3!3m2!1d47.9511462!2d13.788469699999998!4m0!5e1!3m2!1sen!2sat!4v1533618732198"
           FrameBorder "0"
-          Style [ Width "600px"; Height "450px"; Border "0" ]
+          Style [ MaxWidth "600px"; Width "100%"; Height "450px"; Border "0" ]
           !!("allowFullScreen", ()) ]
         [ ] ]
