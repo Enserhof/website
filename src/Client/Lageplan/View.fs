3c4197d7d4b1428c9e348971c0ae95837e92fe0a
Johannes Egger <j.egger@posteo.at>
2018-08-12 07:48:15 +0200

    Preserve aspect ratio of maps

--- src/Client/Lageplan/View.fs
+++ src/Client/Lageplan/View.fs
@@ -5,11 +5,24 @@ open Fable.Helpers.React
 open Fable.Helpers.React.Props
 open Fulma
 
+// https://fettblog.eu/blog/2013/06/16/preserving-aspect-ratio-for-embedded-iframes/
+let aspectRatio percent element =
+  let elementStyle = [
+    Position "absolute"
+    Width "100%"
+    Height "100%"
+    Left "0"
+    Top "0"
+  ]
+  div [ Style [ Position "relative"; Width "100%"; Height "0"; PaddingBottom (sprintf "%d%%" (1. / percent * 100. |> int)) ] ]
+    [ element elementStyle ]
+
 let root =
     [ Heading.h1 [ Heading.Is3 ] [ str "Lageplan" ]
-      iframe
-        [ Src "https://www.google.com/maps/embed?pb=!1m17!1m8!1m3!1d753.3551123114898!2d13.7883404!3d47.9512645!3m2!1i1024!2i768!4f13.1!4m6!3e0!4m3!3m2!1d47.9511462!2d13.788469699999998!4m0!5e1!3m2!1sen!2sat!4v1533618732198"
-          FrameBorder "0"
-          Style [ MaxWidth "600px"; Width "100%"; Height "450px"; Border "0" ]
-          !!("allowFullScreen", ()) ]
-        [ ] ]
+      aspectRatio (16./9.) (fun styles ->
+        iframe
+          [ Src "https://www.google.com/maps/embed?pb=!1m17!1m8!1m3!1d753.3551123114898!2d13.7883404!3d47.9512645!3m2!1i1024!2i768!4f13.1!4m6!3e0!4m3!3m2!1d47.9511462!2d13.788469699999998!4m0!5e1!3m2!1sen!2sat!4v1533618732198"
+            FrameBorder "0"
+            Style (styles @ [ Border "0" ])
+            !!("allowFullScreen", ()) ]
+          [ ] ) ]
