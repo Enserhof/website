0e117466755b36a400e8e4fb36bf637c6b22acc6
Johannes Egger <j.egger@posteo.at>
2021-06-14 09:35:40 +0200

    Update View.fs

--- src/Client/Startseite/View.fs
+++ src/Client/Startseite/View.fs
@@ -13,7 +13,7 @@ let root =
         p [] [
             img [ Src (importAll "../../images/startseite/hof.jpg")?``default``]
         ]
-        p [] [ str "Wir, Johannes und Sylvia, leben mit unseren zwei Kindern auf dem Enserhof im wunderschönen Ehrendorf bei Ohlsdorf - ca. 10 Autominuten von Gmunden entfernt." ]
+        p [] [ str "Wir, Johannes und Sylvia, leben mit unseren zwei Kindern auf dem Enserhof im wunderschönen Ehrendorf bei Ohlsdorf - ca. 5 Autominuten von Gmunden entfernt." ]
         p [] [
           str "Im Laufe der Jahre haben sich immer mehr verschiedene Tiere auf dem Hof eingelebt. "
           str "Es ist uns eine Herzensangelegenheit, dass unsere Tiere ihren grundlegenden Bedürfnissen bei uns auf dem Hof nachkommen können, dass sie mit biologischem Futter gefüttert werden und dass sie genug Platz zum Bewegen haben."
